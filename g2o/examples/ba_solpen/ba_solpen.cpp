0008846df7d0c161440c8a55b65351b192a458ff
shawn_le <shawn_le@solomon-3d.com>
2021-09-17 17:16:52 +0800

    + done read all data

--- g2o/examples/ba_solpen/ba_solpen.cpp
+++ g2o/examples/ba_solpen/ba_solpen.cpp
@@ -39,6 +39,7 @@
 
 #include "g2o/core/optimization_algorithm_factory.h"
 #include "g2o/core/robust_kernel_impl.h"
+#include "g2o/core/io_helper.h"
 #include "g2o/core/sparse_optimizer.h"
 #include "g2o/solvers/structure_only/structure_only_solver.h"
 #include "g2o/stuff/sampler.h"
@@ -116,13 +117,47 @@ int read_rvec_tvec_txtfile(string file, vector<g2o::SE3Quat>& cam_T_balls) {
 }
 
 bool read_aruco_corner_measurements(string folder, vector<vector<Vector2d>>& aru_im_corners) {
+    aru_im_corners.clear();
 
-  if (bfs::is_directory(bfs::path(folder.c_str()))) {
-  } else {
-    cout << folder << " is not a dir\n";
-  }
+    if (bfs::is_directory(bfs::path(folder.c_str()))) {
+        for (auto&& x : bfs::directory_iterator(bfs::path(folder))) {
+            std::size_t found = x.path().filename().string().find("aruco_image_corners_");
+            if (found != std::string::npos) {
+                vector<Vector2d> frm_crns;
 
-  return true;
+                cout << "parsing file " << x.path().string() << "...\n";
+                ifstream infile;
+                infile.open(x.path().string());
+                while (!infile.eof())
+                {
+                    string line;
+                    getline(infile, line);
+
+                    stringstream stream(line);
+                    // int cnt = 0;
+                    // while(! stream.eof()){
+                    //     stream >> buff[cnt];
+                    //     cnt++;
+                    // }
+                    // assert(cnt==1);
+                    // int id = buff[0];
+
+                    Vector2d crns;
+                    g2o::internal::readVector(stream, crns);
+                    frm_crns.push_back(crns);
+                }
+                infile.close();
+
+                aru_im_corners.push_back(frm_crns);
+            }
+
+        }
+    } else {
+        cout << folder << " is not a dir\n";
+        return false;
+    }
+
+    return true;
 }
 
 bool read_aruco_ids(string folder, vector<vector<int>>& aru_ids) {
@@ -139,7 +174,7 @@ bool read_aruco_ids(string folder, vector<vector<int>>& aru_ids) {
 
           vector<int> frm_ids;
 
-          cout << "parsing file " << x.path().string() << '...\n';
+          cout << "parsing file " << x.path().string() << "...\n";
           ifstream infile;
           infile.open(x.path().string());
           while (!infile.eof())
@@ -163,7 +198,7 @@ bool read_aruco_ids(string folder, vector<vector<int>>& aru_ids) {
         }
       }
     } else {
-      cout << folder << " is not a dir\n";
+      std::cout << folder << " is not a dir\n" <<std::endl;
       return false;
     }
     
@@ -274,133 +309,133 @@ int main(int argc, const char* argv[]){
   std::cout << "exit..." << std::endl;
   return 0;
 
-  int vertex_id = 0;
-  for (size_t i=0; i<15; ++i) {
-    Vector3d trans(i*0.04-1.,0,0);
+//   int vertex_id = 0;
+//   for (size_t i=0; i<15; ++i) {
+//     Vector3d trans(i*0.04-1.,0,0);
 
-    Eigen:: Quaterniond q;
-    q.setIdentity();
-    g2o::SE3Quat pose(q,trans);
-    g2o::VertexSE3Expmap * v_se3
-        = new g2o::VertexSE3Expmap();
-    v_se3->setId(vertex_id);
-    if (i<2){
-      v_se3->setFixed(true);
-    }
-    v_se3->setEstimate(pose);
-    optimizer.addVertex(v_se3);
-    true_poses.push_back(pose);
-    vertex_id++;
-  }
-  int point_id=vertex_id;
-  int point_num = 0;
-  double sum_diff2 = 0;
+//     Eigen:: Quaterniond q;
+//     q.setIdentity();
+//     g2o::SE3Quat pose(q,trans);
+//     g2o::VertexSE3Expmap * v_se3
+//         = new g2o::VertexSE3Expmap();
+//     v_se3->setId(vertex_id);
+//     if (i<2){
+//       v_se3->setFixed(true);
+//     }
+//     v_se3->setEstimate(pose);
+//     optimizer.addVertex(v_se3);
+//     true_poses.push_back(pose);
+//     vertex_id++;
+//   }
+//   int point_id=vertex_id;
+//   int point_num = 0;
+//   double sum_diff2 = 0;
 
-  cout << endl;
-  unordered_map<int,int> pointid_2_trueid;
-  unordered_set<int> inliers;
+//   cout << endl;
+//   unordered_map<int,int> pointid_2_trueid;
+//   unordered_set<int> inliers;
 
-  for (size_t i=0; i<mk_corners.size(); ++i){
-    g2o::VertexPointXYZ * v_p
-        = new g2o::VertexPointXYZ();
-    v_p->setId(point_id);
-    v_p->setMarginalized(true);
-    v_p->setEstimate(mk_corners.at(i)
-                     + Vector3d(g2o::Sampler::gaussRand(0., 1),
-                                g2o::Sampler::gaussRand(0., 1),
-                                g2o::Sampler::gaussRand(0., 1)));
-    int num_obs = 0;
-    for (size_t j=0; j<true_poses.size(); ++j){
-      Vector2d z = cam_params->cam_map(true_poses.at(j).map(mk_corners.at(i)));
-      if (z[0]>=0 && z[1]>=0 && z[0]<640 && z[1]<480){
-        ++num_obs;
-      }
-    }
-    if (num_obs>=2){
-      optimizer.addVertex(v_p);
-      bool inlier = true;
-      for (size_t j=0; j<true_poses.size(); ++j){
-        Vector2d z
-            = cam_params->cam_map(true_poses.at(j).map(mk_corners.at(i)));
+//   for (size_t i=0; i<mk_corners.size(); ++i){
+//     g2o::VertexPointXYZ * v_p
+//         = new g2o::VertexPointXYZ();
+//     v_p->setId(point_id);
+//     v_p->setMarginalized(true);
+//     v_p->setEstimate(mk_corners.at(i)
+//                      + Vector3d(g2o::Sampler::gaussRand(0., 1),
+//                                 g2o::Sampler::gaussRand(0., 1),
+//                                 g2o::Sampler::gaussRand(0., 1)));
+//     int num_obs = 0;
+//     for (size_t j=0; j<true_poses.size(); ++j){
+//       Vector2d z = cam_params->cam_map(true_poses.at(j).map(mk_corners.at(i)));
+//       if (z[0]>=0 && z[1]>=0 && z[0]<640 && z[1]<480){
+//         ++num_obs;
+//       }
+//     }
+//     if (num_obs>=2){
+//       optimizer.addVertex(v_p);
+//       bool inlier = true;
+//       for (size_t j=0; j<true_poses.size(); ++j){
+//         Vector2d z
+//             = cam_params->cam_map(true_poses.at(j).map(mk_corners.at(i)));
 
-        if (z[0]>=0 && z[1]>=0 && z[0]<640 && z[1]<480){
-          double sam = g2o::Sampler::uniformRand(0., 1.);
-          if (sam<OUTLIER_RATIO){
-            z = Vector2d(Sample::uniform(0,640),
-                         Sample::uniform(0,480));
-            inlier= false;
-          }
-          z += Vector2d(g2o::Sampler::gaussRand(0., PIXEL_NOISE),
-                        g2o::Sampler::gaussRand(0., PIXEL_NOISE));
-          g2o::EdgeProjectXYZ2UV * e
-              = new g2o::EdgeProjectXYZ2UV();
-          e->setVertex(0, dynamic_cast<g2o::OptimizableGraph::Vertex*>(v_p));
-          e->setVertex(1, dynamic_cast<g2o::OptimizableGraph::Vertex*>
-                       (optimizer.vertices().find(j)->second));
-          e->setMeasurement(z);
-          e->information() = Matrix2d::Identity();
-          if (ROBUST_KERNEL) {
-            g2o::RobustKernelHuber* rk = new g2o::RobustKernelHuber;
-            e->setRobustKernel(rk);
-          }
-          e->setParameterId(0, 0);
-          optimizer.addEdge(e);
-        }
-      }
+//         if (z[0]>=0 && z[1]>=0 && z[0]<640 && z[1]<480){
+//           double sam = g2o::Sampler::uniformRand(0., 1.);
+//           if (sam<OUTLIER_RATIO){
+//             z = Vector2d(Sample::uniform(0,640),
+//                          Sample::uniform(0,480));
+//             inlier= false;
+//           }
+//           z += Vector2d(g2o::Sampler::gaussRand(0., PIXEL_NOISE),
+//                         g2o::Sampler::gaussRand(0., PIXEL_NOISE));
+//           g2o::EdgeProjectXYZ2UV * e
+//               = new g2o::EdgeProjectXYZ2UV();
+//           e->setVertex(0, dynamic_cast<g2o::OptimizableGraph::Vertex*>(v_p));
+//           e->setVertex(1, dynamic_cast<g2o::OptimizableGraph::Vertex*>
+//                        (optimizer.vertices().find(j)->second));
+//           e->setMeasurement(z);
+//           e->information() = Matrix2d::Identity();
+//           if (ROBUST_KERNEL) {
+//             g2o::RobustKernelHuber* rk = new g2o::RobustKernelHuber;
+//             e->setRobustKernel(rk);
+//           }
+//           e->setParameterId(0, 0);
+//           optimizer.addEdge(e);
+//         }
+//       }
 
-      if (inlier){
-        inliers.insert(point_id);
-        Vector3d diff = v_p->estimate() - mk_corners[i];
+//       if (inlier){
+//         inliers.insert(point_id);
+//         Vector3d diff = v_p->estimate() - mk_corners[i];
 
-        sum_diff2 += diff.dot(diff);
-      }
-      pointid_2_trueid.insert(make_pair(point_id,i));
-      ++point_id;
-      ++point_num;
-    }
-  }
-  cout << endl;
-  optimizer.initializeOptimization();
-  optimizer.setVerbose(true);
-  if (STRUCTURE_ONLY){
-    g2o::StructureOnlySolver<3> structure_only_ba;
-    cout << "Performing structure-only BA:"   << endl;
-    g2o::OptimizableGraph::VertexContainer points;
-    for (g2o::OptimizableGraph::VertexIDMap::const_iterator it = optimizer.vertices().begin(); it != optimizer.vertices().end(); ++it) {
-      g2o::OptimizableGraph::Vertex* v = static_cast<g2o::OptimizableGraph::Vertex*>(it->second);
-      if (v->dimension() == 3)
-        points.push_back(v);
-    }
-    structure_only_ba.calc(points, 10);
-  }
-  //optimizer.save("test.g2o");
-  cout << endl;
-  cout << "Performing full BA:" << endl;
-  optimizer.optimize(10);
-  cout << endl;
-  cout << "Point error before optimisation (inliers only): " << sqrt(sum_diff2/inliers.size()) << endl;
-  point_num = 0;
-  sum_diff2 = 0;
-  for (unordered_map<int,int>::iterator it=pointid_2_trueid.begin();
-       it!=pointid_2_trueid.end(); ++it){
-    g2o::HyperGraph::VertexIDMap::iterator v_it
-        = optimizer.vertices().find(it->first);
-    if (v_it==optimizer.vertices().end()){
-      cerr << "Vertex " << it->first << " not in graph!" << endl;
-      exit(-1);
-    }
-    g2o::VertexPointXYZ * v_p
-        = dynamic_cast< g2o::VertexPointXYZ * > (v_it->second);
-    if (v_p==0){
-      cerr << "Vertex " << it->first << "is not a PointXYZ!" << endl;
-      exit(-1);
-    }
-    Vector3d diff = v_p->estimate()-mk_corners[it->second];
-    if (inliers.find(it->first)==inliers.end())
-      continue;
-    sum_diff2 += diff.dot(diff);
-    ++point_num;
-  }
-  cout << "Point error after optimisation (inliers only): " << sqrt(sum_diff2/inliers.size()) << endl;
-  cout << endl;
+//         sum_diff2 += diff.dot(diff);
+//       }
+//       pointid_2_trueid.insert(make_pair(point_id,i));
+//       ++point_id;
+//       ++point_num;
+//     }
+//   }
+//   cout << endl;
+//   optimizer.initializeOptimization();
+//   optimizer.setVerbose(true);
+//   if (STRUCTURE_ONLY){
+//     g2o::StructureOnlySolver<3> structure_only_ba;
+//     cout << "Performing structure-only BA:"   << endl;
+//     g2o::OptimizableGraph::VertexContainer points;
+//     for (g2o::OptimizableGraph::VertexIDMap::const_iterator it = optimizer.vertices().begin(); it != optimizer.vertices().end(); ++it) {
+//       g2o::OptimizableGraph::Vertex* v = static_cast<g2o::OptimizableGraph::Vertex*>(it->second);
+//       if (v->dimension() == 3)
+//         points.push_back(v);
+//     }
+//     structure_only_ba.calc(points, 10);
+//   }
+//   //optimizer.save("test.g2o");
+//   cout << endl;
+//   cout << "Performing full BA:" << endl;
+//   optimizer.optimize(10);
+//   cout << endl;
+//   cout << "Point error before optimisation (inliers only): " << sqrt(sum_diff2/inliers.size()) << endl;
+//   point_num = 0;
+//   sum_diff2 = 0;
+//   for (unordered_map<int,int>::iterator it=pointid_2_trueid.begin();
+//        it!=pointid_2_trueid.end(); ++it){
+//     g2o::HyperGraph::VertexIDMap::iterator v_it
+//         = optimizer.vertices().find(it->first);
+//     if (v_it==optimizer.vertices().end()){
+//       cerr << "Vertex " << it->first << " not in graph!" << endl;
+//       exit(-1);
+//     }
+//     g2o::VertexPointXYZ * v_p
+//         = dynamic_cast< g2o::VertexPointXYZ * > (v_it->second);
+//     if (v_p==0){
+//       cerr << "Vertex " << it->first << "is not a PointXYZ!" << endl;
+//       exit(-1);
+//     }
+//     Vector3d diff = v_p->estimate()-mk_corners[it->second];
+//     if (inliers.find(it->first)==inliers.end())
+//       continue;
+//     sum_diff2 += diff.dot(diff);
+//     ++point_num;
+//   }
+//   cout << "Point error after optimisation (inliers only): " << sqrt(sum_diff2/inliers.size()) << endl;
+//   cout << endl;
 }
