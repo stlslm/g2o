1b9e93bec5df4d6f1bb491b8ef33eaadc50c7014
shawn_le <shawn_le@solomon-3d.com>
2021-09-17 10:58:45 +0800

    + build passed

--- g2o/types/sba/edge_project_aru2uv.cpp
+++ g2o/types/sba/edge_project_aru2uv.cpp
@@ -10,9 +10,18 @@ EdgeProjectARU2UV::EdgeProjectARU2UV() : BaseVariableSizedEdge<33, VertexSE3Expm
 
 }
 
+bool EdgeProjectARU2UV::read(std::istream& is) {
+
+};
+
+bool EdgeProjectARU2UV::write(std::ostream& os) const {
+
+};
+
 void EdgeProjectARU2UV::computeError() {
 
-}
+};
+
 
 
 }  // namespace g2o
